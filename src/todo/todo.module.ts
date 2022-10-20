e0171169e7a6e6d98bff05cb55d90e0caea0a9ab
weslley matos <weslley_mattosibf@hotmail.com>
2022-10-20 15:14:01 -0300

    I added service and controller to todo mod

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { TodoController } from "./controllers/todo.service";
 import { Todo } from "./entities/todo.entity";
+import { TodoService } from "./services/todo.service";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([Todo])],
-    controllers: [],
-    providers: [],
+    controllers: [TodoController],
+    providers: [TodoService],
     exports: [TypeOrmModule]
 })
 export class todoModule { }
