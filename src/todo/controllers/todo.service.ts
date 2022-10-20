efa116764f889c65fe2b601d2412aea5e8f6e571
weslley matos <weslley_mattosibf@hotmail.com>
2022-10-20 15:03:04 -0300

    I created controller and callCreate function

---
+++ src/todo/controllers/todo.service.ts
@@ -0,0 +1,15 @@
+import { Controller } from "@nestjs/common";
+import { Todo } from "../entities/todo.entity";
+import { TodoService } from "../services/todo.service";
+
+@Controller()
+export class TodoController {
+
+    constructor(
+        private readonly todoService: TodoService
+    ) { }
+
+    callCreate(todo: Todo): Promise<Todo> {
+        return this.todoService.create(todo);
+    }
+}
